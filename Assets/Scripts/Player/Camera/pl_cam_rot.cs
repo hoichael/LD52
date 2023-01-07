720e9142c0a4878674b2b231f5b7ed611ad35366
hoichael <31223078+hoichael@users.noreply.github.com>
2023-01-07 11:52:53 +0100

    Basic first person cam rotation

---
+++ Assets/Scripts/Player/Camera/pl_cam_rot.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pl_cam_rot : MonoBehaviour
+{
+    [SerializeField] float sens;
+    [SerializeField] Transform camHolder;
+    [SerializeField] Transform orientation;
+
+    float rotX, rotY;
+
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+    }
+
+    private void Update()
+    {
+        HandleInput();
+        ApplyRotation();
+    }
+
+    private void HandleInput()
+    {
+        rotY += Input.GetAxisRaw("Mouse X") * sens;
+        rotX -= Input.GetAxisRaw("Mouse Y") * sens;
+
+        rotX = Mathf.Clamp(rotX, -89.5f, 89.5f);       
+    }
+
+    private void ApplyRotation()
+    {
+        camHolder.localRotation = Quaternion.Euler(rotX, rotY, 0);
+
+        // technically doesnt belong here but too convenient not to do it
+        orientation.rotation = Quaternion.Euler(0, rotY, 0);
+    }
+}
+
