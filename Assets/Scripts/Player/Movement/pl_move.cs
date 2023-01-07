c1b2b3e4f53ad498ff657e8263d89c9bacff7090
hoichael <31223078+hoichael@users.noreply.github.com>
2023-01-07 12:16:56 +0100

    Basic player movement

---
+++ Assets/Scripts/Player/Movement/pl_move.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pl_move : MonoBehaviour
+{
+    [SerializeField] Rigidbody rb;
+    [SerializeField] float moveForce;
+    [SerializeField] Transform orientation;
+
+    private void FixedUpdate()
+    {
+        ApplyForce(GetMoveDirection());
+    }
+
+    private Vector3 GetMoveDirection()
+    {
+        Vector3 dir =
+            orientation.forward * Input.GetAxisRaw("Vertical") +
+            orientation.right * Input.GetAxisRaw("Horizontal");
+
+        return dir.normalized;
+    }
+
+    private void ApplyForce(Vector3 moveDir)
+    {
+        rb.AddForce(moveDir * moveForce, ForceMode.Acceleration);
+    }
+}
