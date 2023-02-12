7effc23aeaad75374b446ac8936f31f358c4c0f5
hoichael <31223078+hoichael@users.noreply.github.com>
2023-02-12 09:45:59 +0100

    UI for placeholder score screen
    (also move some files)

--- Assets/Scripts/Death Scene/death_scoreboard.cs
+++ Assets/Scripts/Death Scene/death_scoreboard.cs
@@ -7,13 +7,19 @@ public class death_scoreboard : MonoBehaviour
 {
     [Header("Text Elements")]
     [SerializeField] TextMeshPro titleTextEl;
-    [SerializeField] TextMeshPro scoreTextEl, highScoreTextEl_1, highScoreTextEl_2, highScoreTextEl_3, highScoreTextEl_4, highScoreTextEl_5;
+    [SerializeField] TextMeshPro newHighscoreTextEl, scoreTextEl, highScoreTextEl_1, highScoreTextEl_2, highScoreTextEl_3, highScoreTextEl_4, highScoreTextEl_5;
     //[SerializeField] TextMeshPro highScoreTimeTextEl_1, highScoreTimeTextEl_2, highScoreTimeTextEl_3, highScoreTimeTextEl_4, highScoreTimeTextEl_5;
 
     [Header("Other Refs")]
     [SerializeField] pd_score scoreFileHandler;
     [SerializeField] pd_session sessionData;
     [SerializeField] pd_session_RESET dataResetter;
+
+
+    [Header("Values")]
+    [SerializeField] float textPulseAnimSpeed;
+
+    float currentTextPulseAnimFactor;
 
     List<score_data> scoreDataList;
 
@@ -37,6 +43,24 @@ public class death_scoreboard : MonoBehaviour
 
 
         dataResetter.ResetToDefaults();
+    }
+
+    private void Update()
+    {
+        HandleHighscoreTextScale();
+    }
+
+    private void HandleHighscoreTextScale()
+    {
+        currentTextPulseAnimFactor = Mathf.MoveTowards(currentTextPulseAnimFactor, 1, textPulseAnimSpeed * Time.deltaTime);
+
+        newHighscoreTextEl.transform.localScale = Vector3.Lerp(
+            Vector3.one,
+            new Vector3(1.6f, 1.6f, 1.6f),
+            Mathf.PingPong(currentTextPulseAnimFactor, 0.5f)
+            );
+
+        if (currentTextPulseAnimFactor == 1) currentTextPulseAnimFactor = 0;
     }
 
 }
